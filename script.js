40af977985710016f4c926a4032b848bd1e20bff
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2024-01-06 23:50:55 -0500

    adding concats

--- script.js
+++ script.js
@@ -96,8 +96,8 @@ function buyWeapon() {
     currentWeapon ++;
     // or currentWeapon += 1
     goldText.innerText = gold;
-    let newWeapon = weapons;
-    text.innerText = "You now have a new weapon."
+    let newWeapon = weapons[currentWeapon].name;
+    text.innerText = "You now have a " + newWeapon + ".";
   }
 }
 
