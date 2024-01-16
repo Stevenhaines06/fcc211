9dfc253cfe432f5117f3c458b61b64d9bbf31c66
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2024-01-15 22:47:58 -0500

    added ternary operators to functions

--- script.js
+++ script.js
@@ -196,16 +196,22 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
-  health = health -= monsters[fighting].level;
+    health -= getMonsterAttackValue(monsters[fighting].level);
   monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
   healthText.innerText = health;
   monsterHealthText.innerText = monsterHealth;
   if (health <= 0) {
     lose();
   } else if (monsterHealth <= 0) {
-    defeatMonster();
-  }
+    fighting === 2 ? winGame() : defeatMonster();
 }
+
+function getMonsterAttackValue(level) {
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
+    return hit > 0 ? hit : 0;
+}
+
 
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
@@ -222,6 +228,10 @@ function defeatMonster() {
 function lose() {
     update(locations[5]);
 }
+
+function winGame() {
+    update(locations[6]);
+  }
 
 function restart() {
     xp = 0;
