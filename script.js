2a2be85752afd1c8e96b7194669d9ed6523f261b
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2024-01-19 19:06:10 -0500

    last ten percent of text roleplaying game

--- script.js
+++ script.js
@@ -86,6 +86,12 @@ const locations = [{
     "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
     "button functions": [restart, restart, restart],
     text: 'You die. ☠️'
+  }, 
+  { 
+    name: "easter egg", 
+    "button text": ["2", "8", "Go to town square?"], 
+    "button functions": [pickTwo, pickEight, goTown], 
+    text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!" 
   }
 ];
 
@@ -210,6 +216,11 @@ function attack() {
     } else if (monsterHealth <= 0) {
       fighting === 2 ? winGame() : defeatMonster();
     }
+
+    if (Math.random() <= .1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks.";
+        currentWeapon --;
+    }
   }
 
 function getMonsterAttackValue(level) {
@@ -217,6 +228,10 @@ function getMonsterAttackValue(level) {
     console.log(hit);
     return hit > 0 ? hit : 0;
 }
+
+function isMonsterHit() {
+    return Math.random() > .2 || health < 20;
+  }
 
 
 function dodge() {
@@ -249,4 +264,24 @@ function restart() {
     healthText.innerText = health;
     xpText.innerText = xp;
     goTown();
+}
+
+function easterEgg() {
+    update(locations[7]);
+}
+
+
+function pick(guess) {
+    while (numbers.length < 10) {
+        numbers.push(Math.floor(Math.random() * 11))
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n"
+    const numbers = [];
+}
+
+function pickTwo() {
+  pick(2);
+}
+function pickEight() {
+  pick(8);
 }
