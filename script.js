5abb65f52c11b65d797b120fb6ad562075345f5c
Stevenhaines06 <48706369+Stevenhaines06@users.noreply.github.com>
2024-01-20 00:00:34 -0500

    5 percent left

--- script.js
+++ script.js
@@ -277,6 +277,14 @@ function pick(guess) {
     }
     text.innerText = "You picked " + guess + ". Here are the random numbers:\n"
     const numbers = [];
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.includes(guess)) {
+    text.innerText += "Right! You win 20 gold!"
+    gold += 20;
+    goldText.innerText = gold;
+    }
 }
 
 function pickTwo() {
